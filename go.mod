2f0e2aa793b309a45ac5f7a99ebcaffe64f45e86
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2021-02-15 23:09:57 +0800

    add -full

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/caiguanhao/html2png
 go 1.15
 
 require (
+	github.com/chromedp/cdproto v0.0.0-20210211211551-bf465a41a1f9
 	github.com/chromedp/chromedp v0.6.5
 	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf
--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"math"
 	"net/url"
 	"os"
 	"path/filepath"
 	"syscall"
 
+	"github.com/chromedp/cdproto/emulation"
+	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
 	"github.com/skratchdot/open-golang/open"
 	"golang.org/x/term"
@@ -25,15 +28,15 @@ func main() {
 	var inputFilename string
 	var outputFilename string
 	var openFile bool
+	var fullPage bool
 	var verbose bool
 
 	showDevices := flag.Bool("devices", false, "list all mobile devices and exit")
-
 	deviceToUse := devices.NewDeviceByName("iPhone 8")
 	flag.Var(deviceToUse, "device", "device number or name from --devices or custom device properties like:\n"+
 		"-d name=string -d useragent=string -d width=int -d height=int -d scale=float -d landscape=bool -d mobile=bool -d touch=bool")
 	flag.Var(deviceToUse, "d", "short version of --device")
-
+	flag.BoolVar(&fullPage, "full", false, "capture full web page")
 	flag.StringVar(&inputFilename, "i", "", "input HTML file name or HTTP URL, default is stdin")
 	flag.StringVar(&outputFilename, "o", "", `output file name, default is stdout, "-" to use temporary file`)
 	flag.BoolVar(&openFile, "open", false, "open the output file")
@@ -111,7 +114,30 @@ func main() {
 		ctx,
 		chromedp.Emulate(deviceToUse),
 		chromedp.Navigate(inputUrl),
-		chromedp.CaptureScreenshot(&buf),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			// https://github.com/chromedp/examples/blob/master/screenshot/main.go
+			_, _, contentSize, err := page.GetLayoutMetrics().Do(ctx)
+			if err != nil {
+				return err
+			}
+			if fullPage {
+				width, height := int64(math.Ceil(contentSize.Width)), int64(math.Ceil(contentSize.Height))
+				err = emulation.SetDeviceMetricsOverride(width, height, 1, false).Do(ctx)
+				if err != nil {
+					return err
+				}
+				buf, err = page.CaptureScreenshot().WithClip(&page.Viewport{
+					X:      contentSize.X,
+					Y:      contentSize.Y,
+					Width:  contentSize.Width,
+					Height: contentSize.Height,
+					Scale:  deviceToUse.info.Scale,
+				}).Do(ctx)
+			} else {
+				buf, err = page.CaptureScreenshot().Do(ctx)
+			}
+			return err
+		}),
 	)
 	if err != nil {
 		die(err)
